ef778375c35886470d34c1053db9720d5b615564
QKoenig <49294921+QKoenig@users.noreply.github.com>
2026-08-24 19:47:27 -0500

    potato container (#6)

--- src/PickupCarrier.cs
+++ src/PickupCarrier.cs
@@ -4,11 +4,20 @@ using Godot;
 
 public partial class PickupCarrier : Node2D
 {
+    private static readonly StringName ContainerInteraction = "potato_container";
+    private static readonly PackedScene PickupItemScene = GD.Load<PackedScene>(
+        "res://scenes/pickup_item.tscn"
+    );
+
     private Area2D _pickupArea = null!;
     private Area2D _closePickupArea = null!;
     private Node2D _holdPoint = null!;
     private Vector2 _facingDirection = Vector2.Up;
     private PickupItem? _heldItem;
+    private Tween? _shakeTween;
+
+    [Export]
+    public NodePath InteractionTileMapPath { get; set; } = new("../../RoomTiles");
 
     [Export(PropertyHint.Range, "1,360,1,degrees")]
     public float PickupConeDegrees { get; set; } = 140.0f;
@@ -55,19 +64,88 @@ public partial class PickupCarrier : Node2D
     {
         if (HeldItem is PickupItem heldItem)
         {
+            if (IsFacingInteractionTile(ContainerInteraction))
+            {
+                ShakeHeldItem(heldItem);
+                return false;
+            }
+
             _heldItem = null;
             heldItem.Throw(_facingDirection * ThrowForce);
             return true;
         }
 
         PickupItem? target = FindBestPickup();
-        if (target is null || !target.TryPickUp(_holdPoint, PickupDuration))
+        if (target is not null)
+        {
+            if (!target.TryPickUp(_holdPoint, PickupDuration))
+            {
+                return false;
+            }
+
+            _heldItem = target;
+            return true;
+        }
+
+        return TryInteractWithTile();
+    }
+
+    private bool TryInteractWithTile()
+    {
+        if (!TryGetFacingInteractionTile(ContainerInteraction, out TileMapLayer tileMap, out Vector2I cell))
         {
             return false;
         }
 
-        _heldItem = target;
+        PickupItem potato = PickupItemScene.Instantiate<PickupItem>();
+        tileMap.GetParent().AddChild(potato);
+        potato.GlobalPosition = tileMap.ToGlobal(tileMap.MapToLocal(cell));
+        if (!potato.TryPickUp(_holdPoint, PickupDuration))
+        {
+            potato.QueueFree();
+            return false;
+        }
+
+        _heldItem = potato;
         return true;
+    }
+
+    private bool IsFacingInteractionTile(StringName interactionType)
+    {
+        return TryGetFacingInteractionTile(interactionType, out _, out _);
+    }
+
+    private bool TryGetFacingInteractionTile(
+        StringName interactionType,
+        out TileMapLayer tileMap,
+        out Vector2I cell
+    )
+    {
+        tileMap = GetNodeOrNull<TileMapLayer>(InteractionTileMapPath);
+        cell = default;
+        if (tileMap is null)
+        {
+            return false;
+        }
+
+        Vector2 tileSize = tileMap.TileSet.TileSize;
+        Vector2 tilePosition = GlobalPosition
+            + _facingDirection * Mathf.Max(tileSize.X, tileSize.Y);
+        cell = tileMap.LocalToMap(tileMap.ToLocal(tilePosition));
+        TileData? tileData = tileMap.GetCellTileData(cell);
+        return tileData is not null
+            && tileData.GetCustomData("interaction_type").AsString() == interactionType;
+    }
+
+    private void ShakeHeldItem(PickupItem heldItem)
+    {
+        _shakeTween?.Kill();
+        _shakeTween = heldItem.CreateTween();
+        _shakeTween.SetTrans(Tween.TransitionType.Sine);
+        _shakeTween.SetEase(Tween.EaseType.InOut);
+        _shakeTween.TweenProperty(heldItem, new NodePath("rotation"), 0.12f, 0.05f);
+        _shakeTween.TweenProperty(heldItem, new NodePath("rotation"), -0.12f, 0.1f);
+        _shakeTween.TweenProperty(heldItem, new NodePath("rotation"), 0.0f, 0.05f);
     }
 
     private PickupItem? FindBestPickup()
